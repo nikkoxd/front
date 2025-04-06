4e93b7c31e9d11a94a2c404c600ec40f62acef95
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-04-06 19:52:07 +0400

    fix: add partners

--- app/routes/partners.tsx
+++ app/routes/partners.tsx
@@ -1,5 +1,6 @@
 import Page from "~/components/page";
 import type { Route } from "./+types/partners";
+import ResidentCard from "~/components/residentcard";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -10,7 +11,15 @@ export function meta({ }: Route.MetaArgs) {
 export default function Partners() {
   return (
     <Page title="Партнеры">
-      Партнеры
+      <div className="grid grid-cols-3 gap-[40px]">
+        <ResidentCard image="/partners/1.png" alt="Partner 1" />
+        <ResidentCard image="/partners/2.png" alt="Partner 2" />
+        <ResidentCard image="/partners/3.png" alt="Partner 3" />
+        <ResidentCard image="/partners/4.png" alt="Partner 4" />
+        <ResidentCard image="/partners/5.png" alt="Partner 5" />
+        <ResidentCard image="/partners/6.png" alt="Partner 6" />
+        <ResidentCard image="/partners/7.png" alt="Partner 7" />
+      </div>
     </Page>
   )
 }
