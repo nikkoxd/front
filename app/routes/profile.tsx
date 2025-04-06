07cdd1e22e142b6ec4bef57adf3938878bab96b2
nikko <35370166+nikkoxd@users.noreply.github.com>
2025-04-06 19:57:10 +0400

    fix: add profile

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -3,14 +3,19 @@ import type { Route } from "./+types/profile";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "Профиль - Титановая Долина" },
+    { title: "Личный кабинет - Титановая Долина" },
   ];
 }
 
 export default function Profile() {
   return (
-    <Page title="Профиль">
-      Профиль
+    <Page title="Личный кабинет">
+      <form className="flex flex-col gap-[32px] w-1/3">
+        <input type="text" placeholder="Имя" className="border-b-1 border-black pb-[8px]" />
+        <input type="text" placeholder="Телефон" className="border-b-1 border-black pb-[8px]" />
+        <input type="email" placeholder="Почта" className="border-b-1 border-black pb-[8px]" />
+        <input type="password" placeholder="Пароль" className="border-b-1 border-black pb-[8px]" />
+      </form>
     </Page>
   )
 }
